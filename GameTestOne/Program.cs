6584d28eb753f8d5f0470c7424c23abbe216649d
Danila <dan_crysis@mail.ru>
2022-07-10 23:23:28 +0300

    Есть тело, но нет движения

--- GameTestOne/Program.cs
+++ GameTestOne/Program.cs
@@ -8,11 +8,13 @@ namespace GameTestOne
 {
     class Program
     {
+       public enum move { up, down, left, right };
+       public move dir = move.up;
         static void Main(string[] args)
         {
+        
             int origRow;
             int origCol;
-
              void WriteAt(string s, int x, int y)
             {
                 try
@@ -90,9 +92,86 @@ namespace GameTestOne
             }
 
 
+            void Draw()
+            {
+                int X = 0;
+                int Y = 0;
+               Console.SetCursorPosition(X, Y);
+                Console.Write("0");
+            }
+            Draw();
+            
+           void Input()
+            {
+                if(Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey();
+                    switch (key.KeyChar)
+                    {
+                        case "W":
+                            dir = move.up;
+                        case "S":
+                            dir = move.down;
+                        case "A":
+                            dir = move.left;
+                        case "D":
+                            dir = move.right;
+
+                    }
+                }
+            }
+
+            void Logic()
+            {
+                if (dir == move.up)
+                {
+
+                }
+            }
 
 
+            /*    int[,] pic = new int[,]
+            {
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, },
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
+            {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, },
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, },
+            };
+
+                  void PrintImage(int[,] image)
+            {
+                for (int i = 0; i < image.GetLength(0); i++)
+                {
+                    for (int j = 0; j < image.GetLength(1); j++)
+                    {
+                        // Console.Write($"{image[i, j]} ");
+                        if (image[i,j] == 0) Console.Write($" ");
+                        else Console.Write($"+");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+
+
+                PrintImage(pic);
+
+           */
             WriteAt("All done!", 0, 20);
+            Console.WriteLine();
+
             Console.WriteLine();
 
             Console.Write("Press <Enter> to exit... ");
